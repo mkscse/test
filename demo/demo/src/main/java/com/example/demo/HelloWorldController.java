1e3624cb3754205f54eede9fc48ec6fb3dcbf476
mkscse <mks.cse@outlook.com>
2023-12-30 17:42:30 +0530

    changed

--- demo/demo/src/main/java/com/example/demo/HelloWorldController.java
+++ demo/demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello javaTpoint";
+    @GetMapping(path = "/hello", produces= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getData() {
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        data.put("key2", "value2");
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
+
 }
